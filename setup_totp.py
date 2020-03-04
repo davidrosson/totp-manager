79e3364a508828c9e2171f376f148765173ea821
David Rosson <david.rosson@gmail.com>
2020-03-05 00:34:21 +0200

    Add code.

---
+++ setup_totp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='totp-manager',
+    version='1.0',
+    description='A tool for generating time-based one-time passwords (TOTP) for services requiring multi-factor authentication (MFA), and for encrypting/managing the underlying MFA secret keys.',
+    author='David M. Rosson',
+    author_email='david.rosson@gmail.com',
+    packages=find_packages(),
+    scripts=['totp-encryptor', 'totp'],
+    install_requires=['pycrypto', 'pyotp', 'pyperclip']
+)
