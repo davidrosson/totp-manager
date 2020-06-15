c6f600a0aeb4521f184cbbee9ffd74c73e455f66
David Rosson <david.rosson@gmail.com>
2020-06-15 08:21:54 +0300

    Python 3.8 compatibility: - Switch from 'pycrypto' to 'pycryptodome'. - Use 'utf-8' encoding and decoding.

--- setup_totp.py
+++ setup_totp.py
@@ -4,11 +4,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='totp-manager',
-    version='2.0',
+    version='2.0.1',
     description='A tool for generating time-based one-time passwords (TOTP) for services requiring multi-factor authentication (MFA), and for encrypting/managing the underlying MFA secret keys.',
     author='David M. Rosson',
     author_email='david.rosson@gmail.com',
     packages=find_packages(),
     scripts=['totp-encryptor', 'totp'],
-    install_requires=['pycrypto', 'pyotp', 'pyperclip']
+    install_requires=['pycryptodome', 'pyotp', 'pyperclip']
 )
